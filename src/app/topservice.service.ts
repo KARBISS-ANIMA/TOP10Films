9f1164db660073da3d0e8b6569bd6d2a9f3eaf30
Vitaliy Matvienko <150621268+KARBISS-ANIMA@users.noreply.github.com>
2024-03-20 01:01:20 +0300

    my changes

--- src/app/topservice.service.ts
+++ src/app/topservice.service.ts
@@ -16,7 +16,7 @@ export class TopserviceService {
     return this.http.get(`${environment.api}/3/movie/popular?language=ru-EU&page=1`, {
       headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZGMzZGE2ZTk0MTMwYjcwNWZkMzI0Nzg3MDY5ZjdjNiIsInN1YiI6IjY1ZjQ3YTkxYWE3ODk4MDE3ZGI3YzA2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eS9D6RS_D0s-63YxoS3z9hWHTAMZ2SQEaIevd8dRbAU'
+        Authorization: `Bearer ${environment.apikey}`
       }}).pipe(
         map((response) => {
           return response as filmListInterface;
