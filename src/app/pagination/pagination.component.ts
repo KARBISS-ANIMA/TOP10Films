d4da8ea6f948b9654869ccc3efccdcdf765145bb
Vitaliy Matvienko <150621268+KARBISS-ANIMA@users.noreply.github.com>
2024-04-01 22:49:38 +0300

    my changes

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -5,7 +5,8 @@ import {films} from "../module/Interface";
 import {ActivatedRoute, RouterOutlet} from "@angular/router";
 import {FormGroup, ReactiveFormsModule} from "@angular/forms";
 import { Router } from '@angular/router';
-import {tap} from "rxjs";
+import {__await} from "tslib";
+
 
 
 
@@ -36,26 +37,30 @@ export class PaginationComponent implements OnInit {
   page :any
 
 
-
-
   title: WritableSignal<films[]>=signal([]);
 
 
   ngOnInit() {
+    this.getPageNumber().then(()=>{ return console.log('hello3')})
+    console.log('hello1')
   this.route.queryParams.subscribe(params =>{
     this.page = +params['page']})
-    this.updateVisiblePage()
+    this.updateVisiblePage().then(()=>{return console.log('update visible page over')})
   }
 
+  async getPageNumber(){
+    await this.updateVisiblePage();
+    return console.log('hello2')
+  }
   public selectPage(page: number){
     this.page = page;
-    this.router.navigate(['/paginate'], {queryParams:{page: this.page}})
-    this.updateVisiblePage();
+    this.router.navigate(['/paginate'], {queryParams:{page: this.page}});
+    this.updateVisiblePage().then(()=>{return console.log('update visible page over')});
   }
-  updateVisiblePage(){
-  this.servis.getPageFilmList(this.page).subscribe((filmDataTitle)=>{
-  this.title.set(filmDataTitle.results);
-   });
+ async updateVisiblePage(){
+  this.servis.getPageFilmList(this.page).subscribe( (filmDataTitle)=>{
+   this.title.set(filmDataTitle.results)
+   })
+   return console.log('update visible page')
   }
 }
-
